f311fe872d49c16cdf0e58d323b78f50bac74278
shahidulllah <mdshahidsumon177@gmail.com>
2025-06-27 11:47:03 +0600

    setup

---
+++ app.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import connectToDatabase from "./helpers.mjs";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(<h2>Welcome to Docker!</h2>);
+});
+
+await connectToDatabase();
+
+app.listen(5000);
